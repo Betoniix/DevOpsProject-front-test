44c343d46df16e11c034262b1ce5f461b48751e7
EmaRCB <emanuelrcb@outlook.com>
2024-05-10 21:45:31 -0600

    DevOpsProject-front-1651: creación del componente de rutas

---
+++ src/app/rutas-table/rutas-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rutas-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './rutas-table.component.html',
+  styleUrl: './rutas-table.component.css'
+})
+export class RutasTableComponent {
+
+}
