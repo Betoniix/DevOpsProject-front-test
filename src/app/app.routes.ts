65390ef5f3bf4897d6c2233d2c7adad1896fbc87
EmaRCB <emanuelrcb@outlook.com>
2024-05-10 22:39:47 -0600

    feature/DevOpsProject-front-8382: creacion del dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CrudTableComponent } from './crud-table/crud-table.component';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
    { path: '', component: LoginComponent },
    { path: 'manage', component: CrudTableComponent },
-   { path: 'register', component: RegisterComponent }
+   { path: 'register', component: RegisterComponent },
+   { path: 'dashboard', component: DashboardComponent}
 ];
