7c3250246f6bc076298c9e672cbd5412c0942732
Mike <miguelhumberto522@gmail.com>
2024-05-12 23:47:43 -0600

    devopsproject-front-0756: NavBar añadido
    Signed-off-by: Mike <miguelhumberto522@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterModule],
+  imports: [RouterOutlet, RouterLink, RouterModule, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'devops-frontend';
