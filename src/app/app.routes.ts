ff8e65563de6281cf41e0c969b97a392898c2665
Victor Mendoza <vmendoza2002@hotmail.com>
2024-05-12 19:03:20 -0600

    DevOpsProject-front-6085: se añadio ruta a CRUD de vehiculos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CrudTableComponent } from './crud-table/crud-table.component';
 import { RegisterComponent } from './register/register.component';
+import {CarCrudTableComponent} from "./car-crud-table/car-crud-table.component";
 
 export const routes: Routes = [
    { path: '', component: LoginComponent },
    { path: 'manage', component: CrudTableComponent },
-   { path: 'register', component: RegisterComponent }
+   { path: 'register', component: RegisterComponent },
+   { path: 'cars', component: CarCrudTableComponent }
 ];
