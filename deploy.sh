592d564c29cc5cf446549c6e92f4bdfb9a9af8f6
Mike <miguelhumberto522@gmail.com>
2024-05-28 23:57:53 -0600

    dad
    Signed-off-by: Mike <miguelhumberto522@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,10 @@ if [ -d "DevOpsProject-front" ]
 then
     echo existe
     cd DevOpsProject-front && git pull
-    cd ..
 else
     git clone https://github.com/CL-Nayib/DevOpsProject-front.git
 fi
 
-echo enviando
-ls
-sshpass -p "1" scp -r ./DevOpsProject-front ubu@192.168.0.11:/home/ubu/Documents
+npm i && npm run build
+
+#sshpass -p "1" scp -r ./DevOpsProject-front ubu@192.168.0.11:/home/ubu/Documents
