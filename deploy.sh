615b6c2ad83801f8753eab3d501cd09e7bfd63d0
Mike <miguelhumberto522@gmail.com>
2024-05-29 00:15:57 -0600

    dist
    Signed-off-by: Mike <miguelhumberto522@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ then
     cd DevOpsProject-front && git pull
 else
     git clone https://github.com/CL-Nayib/DevOpsProject-front.git
+    cd DevOpsProject-front
 fi
 
 npm i && npm run build
-ls
-#sshpass -p "1" scp -r ./DevOpsProject-front ubu@192.168.0.11:/home/ubu/Documents
+sshpass -p "1" scp -r ./dist ubu@192.168.0.11:/home/ubu/Documents
